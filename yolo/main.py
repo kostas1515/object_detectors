bb137516ce670f985b8ae30a76d02dd4dcbc85d4
kostas1515 <konsa15@liverpool.ac.uk>
2021-02-02 15:58:58 +0000

    add validation loss

--- yolo/main.py
+++ yolo/main.py
@@ -8,6 +8,7 @@ import torch.distributed as dist
 from nets.yolo_forw import YOLOForw
 from procedures.init_dataset import get_dataloaders
 from procedures.train_one_epoch import train_one_epoch
+from procedures.valid_one_epoch import valid_one_epoch
 from procedures.test_one_epoch import test_one_epoch
 from procedures.eval_results import eval_results,save_results
 from procedures.initialize import save_model,get_model
@@ -49,26 +50,33 @@ def main(cfg: DictConfig) -> None:
     #criterion
     criterion = YOLOForw(cfg['yolo'])
 
-
-    epochs=60
+    epochs=1
+    batch_loss = torch.zeros(1)
     for i in range(epochs):
-        if cfg.only_test is False:
+        if cfg.only_test is True:
+            if cfg.metrics =='mAP':
+                results=test_one_epoch(test_loader,model,criterion)
+                save_results(results,cfg.rank)
+                dist.barrier()
+                if cfg.rank==0:
+                    mAP=eval_results(i+last_epoch,dset_config['dset_name'],dset_config['val_annotations'])
+                    print(f'map is {mAP}')
+                    return mAP
+            else:
+                batch_loss = valid_one_epoch(test_loader,model,criterion,cfg.rank)
+                dist.all_reduce(batch_loss, op=torch.distributed.ReduceOp.SUM, async_op=False)
+                batch_loss = batch_loss / len(test_loader.dataset)
+                if cfg.rank==0:
+                    print(f'batch_loss is {batch_loss}')
+                    return batch_loss
+        else:
             train_one_epoch(train_loader,model,optimizer,criterion,cfg.rank)
-        results=test_one_epoch(test_loader,model,criterion)
-        save_results(results,cfg.rank)
-        dist.barrier()
-        if cfg.rank==0:
-            mAP=eval_results(i+last_epoch,dset_config['dset_name'],dset_config['val_annotations'])
-            if cfg.only_test is False:
-                save_model(model,optimizer,mAP,i+last_epoch,'last')
-                if mAP>mAP_best:
-                    save_model(model,optimizer,mAP,i,'best')
-                    mAP_best=mAP
-            print(f'map is {mAP}')
-
+            save_model(model,optimizer,mAP,i+last_epoch,'last')
+            if mAP>mAP_best:
+                save_model(model,optimizer,mAP,i,'best')
+                mAP_best=mAP
 
     cleanup()
-    return mAP
 
 if __name__=='__main__':
     main()
---
+++ yolo/procedures/valid_one_epoch.py
@@ -0,0 +1,25 @@
+import sys
+from apex import amp
+import numpy as np
+import torch
+import torch.distributed as dist
+
+def valid_one_epoch(dataloader,model,yolo_loss,rank):
+    model.eval()
+    batch_loss=torch.zeros(1)
+    torch.backends.cudnn.benchmark = True
+    counter = 0 
+    with torch.no_grad():
+        for imgs,targets in dataloader:
+            for param in model.parameters():
+                param.grad = None
+            targets = [{k: v.to('cuda') for k, v in t.items()} for t in targets]
+            out=model(imgs.cuda())
+            counter = counter + 1
+            outcome=yolo_loss(out,targets)
+            batch_loss= batch_loss + outcome[0]
+            
+            if (rank == 0) &((counter % 10) ==0):
+                print(batch_loss)
+
+        return batch_loss
